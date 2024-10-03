152b3759d0800df3316feaa6dfdeff55e1bf9983
mshari almutairi <37345524+c9b@users.noreply.github.com>
2024-10-03 22:59:27 +0300

    Update js.js

--- js.js
+++ js.js
@@ -183,10 +183,22 @@ function resetGame() {
     document.getElementById('resultsTable').innerHTML = ''; // تفريغ الجدول
     updateProgressBars();
     
+    // إغلاق النافذة المنبثقة (إذا كانت مفتوحة)
+    const winnerModal = $('#winnerModal');
+    if (winnerModal.hasClass('show')) {
+        winnerModal.modal('hide');
+    }
+    
     // إظهار نموذج إدخال أسماء الفرق
     document.getElementById('teamNamesForm').style.display = 'block';
     document.getElementById('resultsSection').style.display = 'none'; // إخفاء قسم النتائج
 }
+
+// إضافة مستمع للحدث على زر إعادة تعيين في النافذة المنبثقة
+document.getElementById('resetModalBtn').addEventListener('click', function() {
+    resetGame(); // استدعاء دالة resetGame
+    $('#winnerModal').modal('hide'); // إغلاق النافذة المنبثقة
+});
 
 function deleteRow(button, points1, points2) {
     totalPoints1 -= points1;
